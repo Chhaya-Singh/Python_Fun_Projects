e8280e23bbfd032aac16940f3b4541e88d609a1b
Chhaya-Singh <111227196+Chhaya-Singh@users.noreply.github.com>
2023-01-09 23:29:59 +0530

    Add files via upload
    Build Your Own News Paper Anchor and stay updated with all current affairs with this python program👍

---
+++ NewsPaperReader.py
@@ -0,0 +1,22 @@
+# Personal news reader😎
+import requests
+import json
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    speak("News for today.. Lets begin")
+    url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=bdd10edbf99d4cda8384e6dee1a47276"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    for article in arts:
+        speak(article['title'])
+        print(article['title'])
+        speak("Moving on to the next news..Listen Carefully")
+
+    speak("Thanks for listening...")
+
